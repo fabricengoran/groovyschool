eb0a815fc8e99526bf2b03a051ed65b30a7a2238
PF_NGORAN <fabricengoran9@gmail.com>
2020-09-04 12:39:06 +0100

    Update my forked branch with master

---
+++ bin/main/groovyschool/shared/Course.groovy
@@ -0,0 +1,23 @@
+package groovyschool.shared
+
+import grails.compiler.GrailsCompileStatic
+
+@GrailsCompileStatic
+class Course implements Serializable {
+
+    private static final long serialVersionUID = 1
+
+    String title
+    String author
+    String subTitle
+    String image_url
+    String description
+    Date datePublished
+    String status = "draft" //draft, published, archived 
+
+    static constraints = {
+        title nullable: false, blank: false, unique: true
+        author nullable: false, blank: false
+    }
+
+}
